0da8c8813b206847de319271fcf451739a51edad
Saule BN <beyonce@thinkful.com>
2017-06-30 10:54:42 -0400

    initial files and apicheeses

--- server/index.js
+++ server/index.js
@@ -4,7 +4,27 @@ const express = require('express');
 const app = express();
 
 // API endpoints go here!
-
+app.get('/api/cheeses', (req, res) =>{
+    res.json([
+        'Bath Blue',
+        'Barkham Blue',
+        'Buxton Blue',
+        'Cheshire Blue',
+        'Devon Blue',
+        'Dorset Blue Vinney',
+        'Dovedale',
+        'Exmoor Blue',
+        'Harbourne Blue',
+        'Lanark Blue',
+        'Lymeswold',
+        'Oxford Blue',
+        'Shropshire Blue',
+        'Stichelton',
+        'Stilton',
+        'Blue Wensleydale',
+        'Yorkshire Blue'
+    ]);
+});
 
 // Serve the built client
 app.use(express.static(path.resolve(__dirname, '../client/build')));
